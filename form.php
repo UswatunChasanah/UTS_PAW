316699c9aa8ea37ca8c7a08328351db3471efb55
UswatunChasanah <uswatun41100.pesonaxxi@gmail.com>
2022-04-23 13:30:19 +0700

    menambahkan file form.php

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tambah data</title>
+</head>
+<body>
+	<h2>Tambah Data</h2>
+	<form action="action.php" method="POST">
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama_100"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="text" name="email_100"></td>
+			</tr>
+			<tr>
+				<td><button type="submit">Tambah</button></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
