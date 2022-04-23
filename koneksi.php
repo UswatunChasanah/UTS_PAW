fb3aa87e931db33339049cf1573058d1ccf88334
UswatunChasanah <uswatun41100.pesonaxxi@gmail.com>
2022-04-23 13:31:07 +0700

    menambahkan file koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$db = "db_uswatun";
+
+	$koneksi = mysqli_connect($host,$user,$password,$db);
+
+	// if (!$koneksi) {
+	// 	echo "gagal";
+	// }
+	// else{
+	// 	echo "berhasil";
+	// }
+ ?>
